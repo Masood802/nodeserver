f05c52a37eb02f60a4efd0c6646811e5eb997d48
Malik Masood <masoodsse@gmail.com>
2024-05-28 16:38:51 +0500

    made endpoint for post requests

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const axios = require('axios').default;
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const port = process.env.port || 5000;
 app = express();
 app.use(cors());
 app.use(express.json());
-const port = process.env.port || 5000;
+app.use(bodyParser.json());
 const timeout = ms => new Promise(res => setTimeout(res, ms))
 app.listen(port, () => {
     console.log(`server is running at:${port}`)
@@ -29,15 +31,36 @@ app.get("/", (req, res) => {
     res.send('Welcome to get Dishes Data');
 })
 app.post('/details',async (req,res)=>{
-    const id = 52772;
-    const item = {};
+    const id = req.body.id;
+    let item = {};
     try {
         axios.post(`http://themealdb.com/api/json/v1/1/lookup.php?i=${id}`).then(resp => {
-        console.log('psot data',resp.data)
+        console.log('post data',resp.data)
         item = resp.data
-    })    
+        })
+        await timeout(2000);
+        if (item != null) {
+            res.send(item);    
+        }
+        else {
+            res.send('Id does not axist');
+         }  
     } catch (err) {
         res.status(500).json({ message: 'data not found' });
     }
     
 })
+app.post('/categorywise-list', async(req, res) => {
+    const category = req.body.category;
+    let list = [];
+    try {
+        axios.post(`http://themealdb.com/api/json/v1/1/filter.php?c=${category}`).then(resp => {
+        console.log(resp.data)
+        list = resp.data;
+        })
+        await timeout(2000);
+        res.send(list);
+    } catch (err) {
+        res.status(500).json({ message: 'data not found' });
+    }
+    })
