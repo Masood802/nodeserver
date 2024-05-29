8ae2d730657f34865bc98fdf2aef01cbcecf1830
Malik Masood <masoodsse@gmail.com>
2024-05-29 16:20:17 +0500

    fixed unnecessary code..

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-const timeout = ms => new Promise(res => setTimeout(res, ms))
 app.listen(port, () => {
     console.log(`server is running at:${port}`)
 })
@@ -16,15 +15,11 @@ app.use((req, res, err, next) => {
 })
 //get request to show all categories of meals...
 app.get('/all', async (req, res) => {
-    let data = [];
     try {
         axios.get('http://themealdb.com/api/json/v1/1/categories.php').then(resp => {
-            data = resp.data.categories;
+            return resp.data.categories;
             
         });
-        await timeout(2000);
-        res.send(data)
-        
     } catch (error) {
         next(error);    
     }
@@ -35,13 +30,10 @@ app.get("/", (req, res) => {
 //post request to list of meals in specific category.. 
 app.post('/categorywise-list', async(req, res) => {
     const category = req.body.category;
-    let list = [];
     try {
         axios.post(`http://themealdb.com/api/json/v1/1/filter.php?c=${category}`).then(resp => {
-        list = resp.data;
+        return resp.data;
         })
-        await timeout(2000);
-        res.send(list);
     } catch (error) {
         next(error);
     }
@@ -49,13 +41,10 @@ app.post('/categorywise-list', async(req, res) => {
     //post request to show details of any meal for given id..
 app.post('/details',async (req,res)=>{
     const id = req.body.id;
-    let item = {};
-    try {
+        try {
         axios.post(`http://themealdb.com/api/json/v1/1/lookup.php?i=${id}`).then(resp => {
-        item = resp.data
+        return resp.data
         })
-        await timeout(2000);
-        res.send(item)  
     } catch (error) {
         next(error)
     }    
